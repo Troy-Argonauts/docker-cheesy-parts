89bb6402a19d08ee330d83f533a149b82e3b1f3f
Christian Emmer <emmercm@gmail.com>
2021-06-09 19:41:58 -0400

    DNS commands for Heroku.

--- heroku.sh
+++ heroku.sh
@@ -2,10 +2,11 @@
 set -euo pipefail
 
 APP_NAME=${1:-argo-cheesy-parts}
+APP_DOMAIN=${2:-parts.frcargonauts.org}
 
 if ! heroku apps:info > /dev/null; then
     heroku create argo-cheesy-parts
-    heroku stack:set --app "${APP_NAME}" container
+    heroku stack:set container
 fi
 
 heroku apps:info
@@ -20,3 +21,12 @@ if ! heroku addons | grep -q cleardb; then
         MYSQL_HOST=$(echo "${CLEARDB_DATABASE_URL}" | sed 's/mysql:\/\/[^:]*:[^@]*@\([^\/]*\).*/\1/') \
         MYSQL_DATABASE=$(echo "${CLEARDB_DATABASE_URL}" | sed 's/mysql:\/\/[^:]*:[^@]*@[^\/]*\/\([^?]*\).*/\1/') > /dev/null
 fi
+
+if ! heroku domains | grep -q herokudns.com; then
+    heroku domains:add "${APP_DOMAIN}"
+    heroku domains:wait "${APP_DOMAIN}"
+    heroku domains:info "${APP_DOMAIN}" | grep cname | awk '{print $2}'
+fi
+
+heroku domains
+echo ""
